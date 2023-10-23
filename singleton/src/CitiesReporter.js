7e41fc61ed4c2b23fa647da1cbfef07d0d7f6719
gui <guigon@alunos.utfpr.edu.br>
2023-10-23 10:08:52 -0300

    feat: add comment

--- singleton/src/CitiesReporter.js
+++ singleton/src/CitiesReporter.js
@@ -17,6 +17,7 @@ export default class CitiesReporter {
   // Método estático para chamada do construtuor, responsável pela garantia que a classe tenha apenas uma instância.
   // Este método faz a verificação se a instância de CitiesReporter já existe, caso não exista ela é criada, e no caso de já existir a mesma é retornada:
   static getInstance() {
+    //A privatização de atributos não existe no JS, para isso utilizamos uma estrutura condicional para ter a acesso a criação da instância
     if (!CitiesReporter.instance) {
       // Caso não exista
       CitiesReporter.instance = new CitiesReporter();
